6b6e94254ed83b5352c5f0856c5bc0837715beea
Sanzhar Sergaliev <sanzharsergaliev@MacBook-Air-Sanzhar.local>
2026-06-05 00:24:42 +0500

    pep8 ч2

--- homework/homework_15_2.py
+++ homework/homework_15_2.py
@@ -1,7 +1,8 @@
 # В вашем репозитории уже есть файл data_test/application.log.
 #
 # Напишите функцию, которая принимает тип записи ("ERROR", "WARNING", "INFO"),
-# открывает файл через with open(...), построчно читает его и выводит только строки с переданным типом.
+# открывает файл через with open(...),
+# построчно читает его и выводит только строки с переданным типом.
 # Для проверки вызовите find_log_entries("ERROR").
 
 def find_log_entries(key_word):
@@ -9,5 +10,6 @@ def find_log_entries(key_word):
         for i in message:
             if key_word in i:
                 print(i)
+
 
 find_log_entries("ERROR")
