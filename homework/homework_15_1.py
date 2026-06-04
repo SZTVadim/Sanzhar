fab827120388a8fb4dec6693dcc4fb6702f4bbb4
Sanzhar Sergaliev <sanzharsergaliev@MacBook-Air-Sanzhar.local>
2026-06-05 00:15:01 +0500

    HW-15

---
+++ homework/homework_15_1.py
@@ -0,0 +1,86 @@
+# Домашнее задание: Декораторы, @property и @classmethod
+#
+# ЗАДАНИЕ 1: Декоратор
+# 1) Создайте декоратор log_execution:
+#    - Декоратор должен выводить "Функция запущена" перед выполнением функции
+#    - Выполнять саму функцию
+#    - Выводить "Функция завершена" после выполнения функции
+def log_execution(func):
+    def wrapper(*args, **kwargs):
+        print("Функция запущена")
+
+        f = func(*args, **kwargs)
+        print(f)
+
+        print("Функция завершена")
+        return f
+    return wrapper
+
+# 2) Примените декоратор к функции calculate_sum(a, b):
+#    - Функция должна возвращать сумму двух чисел
+#    - Используйте синтаксис @log_execution
+@log_execution
+def calculate_sum(a, b):
+    return a + b
+
+# 3) Вызовите функцию calculate_sum(5, 3) и посмотрите результат
+calculate_sum(5, 3)
+
+
+# ЗАДАНИЕ 2: @property и @classmethod
+#
+# Cоздайте класс Book (Книга):
+# 1) В __init__ принимайте: title (название), author (автор)
+#    - Создайте приватный атрибут __price (цена), инициализируйте значением 0
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.__price = 0
+
+# 2) Создайте @property для price:
+#    - Геттер должен возвращать __price
+#    - Сеттер должен проверять: если цена < 0, выводить "Ошибка: цена не может быть отрицательной!"
+#    - Сеттер должен проверять: если цена > 10000, выводить "Ошибка: максимальная цена 10000 рублей!"
+#    - Иначе устанавливать значение
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            print("Ошибка: цена не может быть отрицательной!")
+        elif value > 10000:
+            print("Ошибка: максимальная цена 10000 рублей!")
+        else:
+            self.__price = value
+
+# 3) Создайте @classmethod create_from_string:
+#    - Принимает строку в формате "Название|Автор" (например, "Война и мир|Толстой")
+#    - Разделяет строку и создает объект Book
+#    - Возвращает созданный объект
+    @classmethod
+    def create_from_string(cls, string):
+        title, author = string.split('|')
+        return cls(title, author)
+
+# 4) Создайте обычный метод get_info:
+#    - Возвращает строку: "Книга '{title}' автор {author}, цена {price} руб."
+    def get_info(self):
+        return f'Книга {self.title} автор {self.author}, цена {self.price} руб'
+
+# 5) Использование:
+#    - Создайте книгу обычным способом: book1 = Book("1984", "Оруэлл")
+#    - Создайте книгу через @classmethod: book2 = Book.create_from_string("Мастер и Маргарита|Булгаков")
+#    - Установите цены: book1.price = 500, book2.price = 750
+#    - Попробуйте установить неверную цену: book1.price = -100, book1.price = 15000
+#    - Выведите информацию о книгах через get_info() через print
+book1 = Book("1984", "Оруелл")
+book2 = Book.create_from_string("Мастер и Маргарита|Булгаков")
+book1.price = 500
+book2.price = 750
+book1.price = -100
+book1.price = 15000
+print(book1.get_info())
+print(book2.get_info())
---
+++ homework/homework_15_2.py
@@ -0,0 +1,13 @@
+# В вашем репозитории уже есть файл data_test/application.log.
+#
+# Напишите функцию, которая принимает тип записи ("ERROR", "WARNING", "INFO"),
+# открывает файл через with open(...), построчно читает его и выводит только строки с переданным типом.
+# Для проверки вызовите find_log_entries("ERROR").
+
+def find_log_entries(key_word):
+    with open('../data_test/application.log', 'r') as message:
+        for i in message:
+            if key_word in i:
+                print(i)
+
+find_log_entries("ERROR")
