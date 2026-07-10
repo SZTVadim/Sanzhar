8d6aacada041ed3c09090a5f6e254dc5179292b1
Sanzhar Sergaliev <sanzharsergaliev@MacBook-Air-Sanzhar.local>
2026-07-10 20:43:39 +0500

    Fix

--- homework/homework_21.py
+++ homework/homework_21.py
@@ -1,11 +1,10 @@
 # CSS
-search = "input[placeholder='Search']"
-menu_button_chevron = ".oxd-icon.bi-chevron-left"
-dashboard = "a.oxd-main-menu-item.active"
-upgrade_button = "button.oxd-glass-button.orangehrm-upgrade-button"
-
+css_search = "input[placeholder='Search']"
+css_menu_button = ".oxd-main-menu-button"
+css_upgrade_button = ".orangehrm-upgrade-button"
+css_emp_gear = ".bi-gear-fill"
 # XPath
-my_actions_dashboard_widget = "(//*[@class='oxd-sheet oxd-sheet--rounded oxd-sheet--white orangehrm-dashboard-widget'])[2]"
-user_dropdown = "//*[@class='oxd-userdropdown']"
-employee_on_leave_gear = "//*[@class='oxd-icon bi-gear-fill orangehrm-leave-card-icon']"
-distribution_title_engineering = "//span[@title='Engineering']"
+x_dashboard = "//a[@href='/web/index.php/dashboard/index']"
+x_my_actions_widget = "//p[text()='My Actions']/../../.."
+x_user_dropdown = "//li[contains(@class, 'oxd-userdropdown')]"
+x_distribution_title_engineering = "//span[@title='Engineering']"
